a238581c8b0f4a57212e3a7b7b6c6edf7f8696e4
nikitapt63 <60973588+nikitapt63@users.noreply.github.com>
2022-04-17 16:30:57 +0300

    Connect pdb. Add .env.example. Update README.md. Update settings.py

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# car-showroom
+## Setup
+
+The first thing to do is to clone the repository:
+
+```sh
+$ git clone https://github.com/nikitapt63/car-showroom
+$ cd car_showroom
+```
+
+Clone `.env.example` file, rename it to `.env`.
+
+Run `PostgreSQL` docker container localy:
+
+```sh
+$ docker run --name db -p 5432:5432 --env-file ./.env -d postgres:13.3
+```
+
+And run the Django server:
+
+```sh
+$ cd car_showroom
+$ python manage.py runserver
+```
--- car_showroom/car_showroom/settings.py
+++ car_showroom/car_showroom/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Import the environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'car_showroom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
     }
 }
 
