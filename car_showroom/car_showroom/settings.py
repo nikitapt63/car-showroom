749710a5938d7cc7683523dccc0f2e6654a28bc8
nikitapt63 <60973588+nikitapt63@users.noreply.github.com>
2022-04-18 13:33:54 +0300

    Add SKey to .env, delete comment in settings.py

--- car_showroom/car_showroom/settings.py
+++ car_showroom/car_showroom/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure--26*k5n+*1-hn_8-&60e2af)=i6dxe(9#hzkqgbyji9*josy)7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
