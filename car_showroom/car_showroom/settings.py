9edcecfc064eb97f39da30d18ed32a43d11a75cb
nikitapt63 <60973588+nikitapt63@users.noreply.github.com>
2022-04-18 13:28:31 +0300

    Add <DATABASE> & change .env, update README

--- car_showroom/car_showroom/settings.py
+++ car_showroom/car_showroom/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Import the environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--26*k5n+*1-hn_8-&60e2af)=i6dxe(9#hzkqgbyji9*josy)7'
+SECRET_KEY = os.environ.get('SECRET_KEY') #'django-insecure--26*k5n+*1-hn_8-&60e2af)=i6dxe(9#hzkqgbyji9*josy)7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'car_showroom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
     }
 }
 
