f7bd6fb5c0097ceb1fdbb97c59614a4210aa38c1
nikitapt63 <60973588+nikitapt63@users.noreply.github.com>
2022-04-19 13:44:47 +0300

    Create Customer model

--- car_showroom/customer/models.py
+++ car_showroom/customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_name = models.CharField(max_length=50, verbose_name='Surname')
+    balance = models.FloatField(verbose_name='Balance', editable=False)
+
+    def __str__(self):
+        return f'{self.user}, {self.last_name}, {self.balance}'
