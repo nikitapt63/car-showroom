afa2afec20cdd6c7872fa38446617c865fda0fa5
nikitapt63 <60973588+nikitapt63@users.noreply.github.com>
2022-04-19 13:16:32 +0300

    Create Supplier model

--- car_showroom/supplier/models.py
+++ car_showroom/supplier/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Supplier(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    foundation = models.IntegerField(verbose_name='Foundation year')
+
+    def __str__(self):
+        return f'{self.user}, {self.foundation}'
+
+
+
