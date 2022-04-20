4741dc8614947934e7481ad954b1716fbfa2bd61
nikitapt63 <60973588+nikitapt63@users.noreply.github.com>
2022-04-20 18:26:33 +0300

    Edit Customer and Dealership models

--- car_showroom/customer/models.py
+++ car_showroom/customer/models.py
@@ -5,11 +5,11 @@ from cars.models import Car
 
 
 class Customer(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     last_name = models.CharField(max_length=50, verbose_name="Surname")
     balance = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Current balance in $")
-    pref_cars = models.JSONField(default='{"price": ,"brand": ,"model": ,"fuel_type": ,"year": }')
+    pref_cars = models.JSONField(default={"price": '', "brand": '', "model": '', "fuel_type": '', "year": ''})
 
     def __str__(self):
         return f'{self.user}, {self.last_name}, {self.balance}'
--- car_showroom/dealership/models.py
+++ car_showroom/dealership/models.py
@@ -7,10 +7,10 @@ from cars.models import Car
 
 class Dealership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True)
     location = CountryField(blank_label='(select country)', multiple=True)
     balance = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Current balance in $")
-    pref_cars = models.JSONField(default='{"price": ,"brand": ,"model": ,"fuel_type": ,"year": }')
+    pref_cars = models.JSONField(default={"price": '', "brand": '', "model": '', "fuel_type": '', "year": ''})
 
     def __str__(self):
         return f'{self.user}, {self.location}, {self.balance}'
